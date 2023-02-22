0ef97a52cc157caf45caeae7425066376d05b32a
Durgesh <Durgesh.y@drcsystems.com>
2023-02-22 16:47:06 +0530

    Minitus Control work but one case not complete

--- HospitalTask/src/main/java/com/hospitaltask/controller/AvailityController.java
+++ HospitalTask/src/main/java/com/hospitaltask/controller/AvailityController.java
@@ -25,194 +25,234 @@ import com.hospitaltask.response.CustomResponseHandler;
 
 public class AvailityController {
 
-    @Autowired
-    private ModelMapper modelMapper;
-    @Autowired
-    private AvailityRepo availityRepo;
+	@Autowired
+	private ModelMapper modelMapper;
+	@Autowired
+	private AvailityRepo availityRepo;
 
-    @Autowired
-    private SaveSlotRepo repo;
+	@Autowired
+	private SaveSlotRepo repo;
 
-    @Autowired
-    private CreateSlotRepo createSlotRepo;
+	@Autowired
+	private CreateSlotRepo createSlotRepo;
 
-    Availity dtoToAvality(AvailityDto availityDto) {
-        return this.modelMapper.map(availityDto, Availity.class);
-    }
+	Availity dtoToAvality(AvailityDto availityDto) {
+		return this.modelMapper.map(availityDto, Availity.class);
+	}
 
-    @PostMapping("/show")
-    public ResponseEntity<?> availity(@RequestBody @NotNull AvailityDto availityDto) {
+	@PostMapping("/show")
+	public ResponseEntity<?> availity(@RequestBody @NotNull AvailityDto availityDto) {
 
-        /*
-         * Availity save = null; try { if (availityDto.getStartTime() >= 10 &&
-         * availityDto.getEndTime() <= 17 &&
-         * !availityDto.getStartTime().equals(availityDto.getEndTime()) &&
-         * availityDto.getEndTime() != 0) {
-         * availityDto.setSlot(oneDaySlot(availityDto.getStartTime(),
-         * availityDto.getEndTime())); save =
-         * availityRepo.save(dtoToAvality(availityDto)); for (int i = 0; i < 6; i++) {
-         * int increment = 1; LocalDate date2 =
-         * availityDto.getDate().plusDays(increment); availityDto.setDate(date2); save =
-         * availityRepo.save(dtoToAvality(availityDto));
-         *
-         * }
-         *
-         * return CustomResponseHandler.response("Record Saved", HttpStatus.OK, save); }
-         * } catch (Exception e) { e.printStackTrace(); }
-         */
-        System.out.println(slotMethod(availityDto));
-        return CustomResponseHandler.response("select valid time ", HttpStatus.INTERNAL_SERVER_ERROR, null);
-    }
+		/*
+		 * Availity save = null; try { if (availityDto.getStartTime() >= 10 &&
+		 * availityDto.getEndTime() <= 17 &&
+		 * !availityDto.getStartTime().equals(availityDto.getEndTime()) &&
+		 * availityDto.getEndTime() != 0) {
+		 * availityDto.setSlot(oneDaySlot(availityDto.getStartTime(),
+		 * availityDto.getEndTime())); save =
+		 * availityRepo.save(dtoToAvality(availityDto)); for (int i = 0; i < 6; i++) {
+		 * int increment = 1; LocalDate date2 =
+		 * availityDto.getDate().plusDays(increment); availityDto.setDate(date2); save =
+		 * availityRepo.save(dtoToAvality(availityDto));
+		 *
+		 * }
+		 *
+		 * return CustomResponseHandler.response("Record Saved", HttpStatus.OK, save); }
+		 * } catch (Exception e) { e.printStackTrace(); }
+		 */
+		System.out.println(slotMethod(availityDto));
+		return CustomResponseHandler.response("select valid time ", HttpStatus.INTERNAL_SERVER_ERROR, null);
+	}
 
-    public int oneDaySlot(long STARTTIME, long ENDTIME) {
+	public int oneDaySlot(long STARTTIME, long ENDTIME) {
 
-        if (STARTTIME >= 10 && ENDTIME <= 11) return 1;
-        else if (STARTTIME >= 10 && ENDTIME <= 11) return 2;
-        else if (STARTTIME >= 12 && ENDTIME <= 13) return 3;
-        else if (STARTTIME >= 13 && ENDTIME <= 14) return 4;
-        else if (STARTTIME >= 14 && ENDTIME <= 15) return 5;
-        else if (STARTTIME >= 15 && ENDTIME <= 16) return 6;
-        else if (STARTTIME >= 16 && ENDTIME <= 17) return 6;
-        else return 0;
-    }
+		if (STARTTIME >= 10 && ENDTIME <= 11)
+			return 1;
+		else if (STARTTIME >= 10 && ENDTIME <= 11)
+			return 2;
+		else if (STARTTIME >= 12 && ENDTIME <= 13)
+			return 3;
+		else if (STARTTIME >= 13 && ENDTIME <= 14)
+			return 4;
+		else if (STARTTIME >= 14 && ENDTIME <= 15)
+			return 5;
+		else if (STARTTIME >= 15 && ENDTIME <= 16)
+			return 6;
+		else if (STARTTIME >= 16 && ENDTIME <= 17)
+			return 6;
+		else
+			return 0;
+	}
 
-    int slotMethod(AvailityDto createSlotDto) {
-        String startTime = createSlotRepo.getStartTime();
-        String endTime = createSlotRepo.getEndTime();
-        String duration = createSlotRepo.getDuration();
-        System.out.println("startTime   		" + startTime);
-        System.out.println("endTime   		" + endTime);
-        System.out.println("duration   		" + duration);
-        return createSlot(createSlotDto);
-    }
+	int slotMethod(AvailityDto createSlotDto) {
+		String startTime = createSlotRepo.getStartTime();
+		String endTime = createSlotRepo.getEndTime();
+		String duration = createSlotRepo.getDuration();
+		System.out.println("startTime   		" + startTime);
+		System.out.println("endTime   		" + endTime);
+		System.out.println("duration   		" + duration);
+		return createSlot(createSlotDto);
+	}
 
-    int createSlot(AvailityDto createSlotDto) {
+	int createSlot(AvailityDto createSlotDto) {
 
-        int slot = 0;
-        CreateSlot createSlots = createSlotRepo.getLastInsertedValue();
-        System.out.println(createSlots);
-        String STARTTIMEDB = createSlots.getStartTime();
-        String ENDTIMEDB = createSlots.getEndTime();
-        String dTimeDB = createSlots.getDurationMinutes();
+		int slot = 0;
+		CreateSlot createSlots = createSlotRepo.getLastInsertedValue();
+		System.out.println(createSlots);
+		String STARTTIMEDB = createSlots.getStartTime();
+		String ENDTIMEDB = createSlots.getEndTime();
+		String dTimeDB = createSlots.getDurationMinutes();
 
-        int sTime = Integer.parseInt(STARTTIMEDB.substring(0, 2));
-        int sTimeCopy = sTime;
-        int dTime = Integer.parseInt(dTimeDB.substring(0, 2));
-        int endTime = Integer.parseInt(ENDTIMEDB.substring(0, 2));
-        int totalMinutes = (endTime - sTime) * 60;
-        int totalSlot = totalMinutes / dTime;
-        int k = 1, l = 0, m = 1, n = 1, hourIncrement = 0;
-        String startTimeSave = createSlots.getStartTime();
-        int minutessave = 0;
-        int sTimeSave = 0;
+		int sTime = Integer.parseInt(STARTTIMEDB.substring(0, 2));
+		int sTimeCopy = sTime;
+		int dTime = Integer.parseInt(dTimeDB.substring(0, 2));
+		int endTime = Integer.parseInt(ENDTIMEDB.substring(0, 2));
+		int totalMinutes = (endTime - sTime) * 60;
+		int totalSlot = totalMinutes / dTime;
+		int k = 1, l = 0, hourIncrement = 0;
+		int hours = 0;
+		int startMinutes = 0;
+		int endMinutes = dTime;
+		int manage = 0;
+		hours = Integer.parseInt(STARTTIMEDB);
 
-        sTimeSave = Integer.parseInt(startTimeSave);
+		for (int i = 0; i < totalSlot; i++) {
+			SaveSlot saveSlot = new SaveSlot();
 
-        // Logic using drop-down index
-        for (int i = 0; i < totalSlot; i++) {
-            SaveSlot saveSlot = new SaveSlot();
-            if (i != 0) {
-//Minutes check control TODO 
+			if (i != 0) {
 
-                int checkEndTime = dTime * k;
-                int TimeSave = dTime * l;
+				if (endMinutes >= dTime) {
+					startMinutes = endMinutes;
+					endMinutes = dTime + endMinutes;
+				}
 
-                if (checkEndTime >= 60) {
-                    hourIncrement++;
-                    sTimeSave = sTimeCopy + hourIncrement;
-                    System.out.println(sTimeSave + "11");
-                    int minutes = checkEndTime - 60;
-                    minutessave = dTime + minutes;
-                    k = m;
-                    l = n;
+				else {
+					endMinutes = dTime * k;
+					startMinutes = dTime * l;
 
-                    if (minutes == 0) {
+				}
+				if (endMinutes >= 60) {
+					int oldHours = hours;
+					hourIncrement++;
+					hours = sTimeCopy + hourIncrement;
 
-                        saveSlot.setStartTime(String.valueOf(sTimeSave) + ":" + "00");
-                        saveSlot.setEndTime(sTimeSave + ":" + String.valueOf(minutessave));
-                    } else {
-                        if (minutes < 10) {
+					endMinutes = endMinutes - 60;
+					manage = endMinutes;
+					k = 1;
+					l = 1;
 
-                            saveSlot.setStartTime(String.valueOf(sTimeSave) + ":" + "0" + minutes);
-                            saveSlot.setEndTime(sTimeSave + ":" + String.valueOf(minutessave));
+					if (endMinutes == 0) {
 
-                        } else {
-                            saveSlot.setStartTime(String.valueOf(sTimeSave) + ":" + minutes);
-                            saveSlot.setEndTime(String.valueOf(sTimeSave) + ":" + String.valueOf(minutessave));
-                        }
-                    }
+						saveSlot.setStartTime(String.valueOf(hours) + ":" + "00");
+						oldHours++;
+						endMinutes = endMinutes + dTime;
+						saveSlot.setEndTime(hours + ":" + endMinutes);
+						k = 1;
+						l = 1;
+					} else {
+						if (endMinutes < 10) {
 
-                } else {
+							saveSlot.setStartTime(String.valueOf(oldHours) + ":" + startMinutes);
+							oldHours++;
+							saveSlot.setEndTime(hours + ":" + "0" + String.valueOf(endMinutes));
 
-                    saveSlot.setStartTime(String.valueOf(sTimeSave) + ":" + String.valueOf(TimeSave));
-                    int endTimeSave = (dTime * k);
-                    saveSlot.setEndTime(String.valueOf(sTimeSave) + ":" + String.valueOf(endTimeSave));
-                    k = k + 1;
-                    l = l + 1;
+						} else {
+							saveSlot.setStartTime(String.valueOf(oldHours) + ":" + startMinutes);
+							oldHours++;
+							saveSlot.setEndTime(String.valueOf(hours) + ":" + String.valueOf(endMinutes));
+							k = 1;
+							l = 1;
+						}
+					}
 
-                }
-            } else {
+				} else {
 
-                saveSlot.setStartTime(String.valueOf(startTimeSave) + ":" + "00");
-                saveSlot.setEndTime(String.valueOf(sTimeSave) + ":" + String.valueOf(dTime));
-                k = k + 1;
-                l = l + 1;
+					if (endMinutes < 10) {
+						if (startMinutes < 10)
+							saveSlot.setStartTime(String.valueOf(hours) + ":" + "0" + String.valueOf(startMinutes));
+						else
+							saveSlot.setStartTime(String.valueOf(hours) + ":" + String.valueOf(startMinutes));
 
-            }
-            System.out.println(saveSlot.getStartTime());
-            System.out.println(saveSlot.getEndTime());
-            repo.save(saveSlot);
+						saveSlot.setEndTime(String.valueOf(hours) + ":" + String.valueOf(endMinutes));
 
-        }
+						k++;
+						l++;
 
-        int userSTime = 0;
+					} else {
+						if (startMinutes < 10)
+							saveSlot.setStartTime(String.valueOf(hours) + ":" + "0" + String.valueOf(startMinutes));
+						else
+							saveSlot.setStartTime(String.valueOf(hours) + ":" + String.valueOf(startMinutes));
+						saveSlot.setEndTime(String.valueOf(hours) + ":" + String.valueOf(endMinutes));
+						k++;
+						l++;
 
-        String[] arrOfStr1 = createSlotDto.getStartTime().split(":", 2);
-        for (int j = 0; j < arrOfStr1.length; j++) {
+					}
 
-            int datavalue = Integer.parseInt(arrOfStr1[j]);
-            userSTime = userSTime + datavalue;
-        }
+				}
+			} else {
 
-        for (int i = 0; i < totalSlot; i++) {
-            if (sTime == userSTime) {
-                slot++;
-                break;
-            } else {
+				saveSlot.setStartTime(String.valueOf(hours) + ":" + "00");
+				saveSlot.setEndTime(String.valueOf(hours) + ":" + String.valueOf(endMinutes));
+				k++;
+				l++;
 
-                sTime = sTime + Integer.parseInt(dTimeDB);
-                String durtationHours = String.valueOf(sTime);
-                int minute = Integer.parseInt(durtationHours.substring(durtationHours.length() - 2, durtationHours.length()));
+			}
+			System.out.println(saveSlot.getStartTime());
+			System.out.println(saveSlot.getEndTime());
+			repo.save(saveSlot);
 
-                /*
-                 * if (minute >= 60) { sTimeCopy1 = sTimeCopy1 + 1; sTime = sTimeCopy1; }
-                 *
-                 * slot++;
-                 */
-            }
-        }
+		}
 
-        /*
-         * int startTime = Integer.parseInt(STARTTIMEDB.substring(0, 2)); int
-         * durationTime = Integer.parseInt(DURATIONTIMEDB.substring(0, 2)); int endTime
-         * = Integer.parseInt(ENDTIMEDB.substring(0, 2)); int totalMinutes = (endTime -
-         * startTime) * 60; int totalSlot = totalMinutes / durationTime; int
-         * userStartTime = Integer.parseInt(createSlotDto.getStartTime().substring(0,
-         * 2)); int slot = 0; for (int i = 0; i < totalSlot; i++) { String sTime =
-         * String.valueOf(startTime); if (totalMinutes > durationTime) { String
-         * eUsersTime = String.valueOf(userStartTime) + ":" + durationTime; int sData =
-         * 0, userTime = 0; String[] arrOfStr = sTime.split(":", 2); for (int j = 0; j <
-         * arrOfStr.length; j++) { int datavalue = Integer.parseInt(arrOfStr[i]); sData
-         * = sData + datavalue; } String[] arrOfStr1 =
-         * createSlotDto.getStartTime().split(":", 2); for (int j = 0; j <
-         * arrOfStr1.length; j++) { int datavalue = Integer.parseInt(arrOfStr[i]);
-         * userTime = userTime + datavalue; } System.out.println(sData);
-         * System.out.println(userTime); if (sData == userTime) { slot++; break; } else
-         * { sTime = String.valueOf(startTime) + ":" + durationTime; slot++; } }
-         * System.out.println(i); } System.out.println("start time :   " + startTime +
-         * "   endTime		" + endTime);
-         */
-        System.out.println(slot);
-        return slot;
-    }
+		int userSTime = 0;
+
+		String[] arrOfStr1 = createSlotDto.getStartTime().split(":", 2);
+		for (int j = 0; j < arrOfStr1.length; j++) {
+
+			int datavalue = Integer.parseInt(arrOfStr1[j]);
+			userSTime = userSTime + datavalue;
+		}
+
+		for (int i = 0; i < totalSlot; i++) {
+			if (sTime == userSTime) {
+				slot++;
+				break;
+			} else {
+
+				sTime = sTime + Integer.parseInt(dTimeDB);
+				String durtationHours = String.valueOf(sTime);
+				int minute = Integer
+						.parseInt(durtationHours.substring(durtationHours.length() - 2, durtationHours.length()));
+
+				/*
+				 * if (minute >= 60) { sTimeCopy1 = sTimeCopy1 + 1; sTime = sTimeCopy1; }
+				 *
+				 * slot++;
+				 */
+			}
+		}
+
+		/*
+		 * int startTime = Integer.parseInt(STARTTIMEDB.substring(0, 2)); int
+		 * durationTime = Integer.parseInt(DURATIONTIMEDB.substring(0, 2)); int endTime
+		 * = Integer.parseInt(ENDTIMEDB.substring(0, 2)); int totalMinutes = (endTime -
+		 * startTime) * 60; int totalSlot = totalMinutes / durationTime; int
+		 * userStartTime = Integer.parseInt(createSlotDto.getStartTime().substring(0,
+		 * 2)); int slot = 0; for (int i = 0; i < totalSlot; i++) { String sTime =
+		 * String.valueOf(startTime); if (totalMinutes > durationTime) { String
+		 * eUsersTime = String.valueOf(userStartTime) + ":" + durationTime; int sData =
+		 * 0, userTime = 0; String[] arrOfStr = sTime.split(":", 2); for (int j = 0; j <
+		 * arrOfStr.length; j++) { int datavalue = Integer.parseInt(arrOfStr[i]); sData
+		 * = sData + datavalue; } String[] arrOfStr1 =
+		 * createSlotDto.getStartTime().split(":", 2); for (int j = 0; j <
+		 * arrOfStr1.length; j++) { int datavalue = Integer.parseInt(arrOfStr[i]);
+		 * userTime = userTime + datavalue; } System.out.println(sData);
+		 * System.out.println(userTime); if (sData == userTime) { slot++; break; } else
+		 * { sTime = String.valueOf(startTime) + ":" + durationTime; slot++; } }
+		 * System.out.println(i); } System.out.println("start time :   " + startTime +
+		 * "   endTime		" + endTime);
+		 */
+		System.out.println(slot);
+		return slot;
+	}
 }
