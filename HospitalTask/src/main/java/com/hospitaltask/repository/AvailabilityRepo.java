e3693f734d0513cdaaea112a2593458522637964
Durgesh yadav <durgeshyadavgpm16@gmail.com>
2023-03-21 15:19:02 +0530

    Just For Testing
    Just For Testing

--- HospitalTask/src/main/java/com/hospitaltask/repository/AvailabilityRepo.java
+++ HospitalTask/src/main/java/com/hospitaltask/repository/AvailabilityRepo.java
@@ -3,5 +3,5 @@ package com.hospitaltask.repository;
 import com.hospitaltask.entity.Availity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AvailityRepo extends JpaRepository<Availity,Long> {
+public interface AvailabilityRepo extends JpaRepository<Availity,Long> {
 }
